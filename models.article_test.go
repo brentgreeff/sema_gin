4f83821ae5d12f80cd2c48f8ad26f4c76203440e
Brent <brentgreeff@gmail.com>
2020-09-28 22:56:00 +0200

    Add Article and spec.

---
+++ models.article_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestGetAllArticles(t *testing.T) {
+  want := len(articleList)
+
+  if got := len( getAllArticles() ); got != want {
+    t.Errorf("len( getAllArticles() ) = %d, want %d", got, want)
+  }
+
+  for i, v := range getAllArticles() {
+    if v.Content != articleList[i].Content ||
+      v.ID != articleList[i].ID ||
+      v.Title != articleList[i].Title {
+
+      t.Fail()
+      break
+    }
+  }
+}
